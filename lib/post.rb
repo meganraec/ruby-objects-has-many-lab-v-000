4c22d26c369c4ead1697075eb952a382dcaad6bc
Megan Collier <meganraec@live.com>
2018-05-20 21:01:34 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
 class Post
-  attr_accessor :title, :author
+  attr_accessor :title, :author_name
 
     @@post_count = 0
 
   def initialize(title)
     @title = title
-    @author_name = author
+    @author_name = author_name
     @@post_count += 1
   end
 end
