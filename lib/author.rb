6a7a7ab86078c4dc77d73aafcefe022ef32dee89
Megan Collier <meganraec@live.com>
2018-05-20 20:34:23 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,7 +18,7 @@ class Author
   def add_post_by_title(post_title)
     new_post = Post.new(post_title)
     @posts << new_post
-    new_post_author = self
+    new_post.author = self
   end
 
   def post_count
