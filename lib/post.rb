6caef409bbb6e0179f51041d90828b4e44836141
Megan Collier <meganraec@live.com>
2018-05-20 21:52:33 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,9 @@
 class Post
   attr_accessor :title, :author
 
-# Counts all of the posts by an author.
-    @@post_count = 0
-
   def initialize(title)
     @title = title
     @author = author
-    @@post_count += 1
   end
 
 # Returns the name of the author for the post on which the method is called; returns nil if no author is named.
