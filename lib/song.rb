e89ac4ad80a3dc8f9f985697fef7b51cdc9660ed
Megan Collier <meganraec@live.com>
2018-05-20 21:50:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,9 @@
 class Song
   attr_accessor :name, :artist
 
-# Counts all of the songs by a given artist.
-  @@song_count = 0
-
   def initialize(name)
     @name = name
     @artist = artist
-    @@song_count += 1
   end
 
 # Returns the name of the artist for the song on which the method is called; returns nil if no artist is named.
